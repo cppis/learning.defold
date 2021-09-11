6bd1f6853465b0c2f1ee36e02fd9f1c7bbe16e10
cppis <cppis@daum.net>
2021-09-11 20:28:02 +0900

    fix alien world

---
+++ alien-world/player/player.go
@@ -0,0 +1,30 @@
+components {
+  id: "player/camera"
+  component: "/alien-world/player/player.camera"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "player/script"
+  component: "/alien-world/player/player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
